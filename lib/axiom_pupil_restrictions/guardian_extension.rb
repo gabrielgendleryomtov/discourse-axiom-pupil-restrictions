90ea05eb890ba023556174c45d31b7eedb7102e7
Gabriel Gendler Yom-Tov <gabriel.gendler@gmail.com>
2026-02-23 14:22:28 +0000

    Allow staff to edit pupil usernames and names

--- lib/axiom_pupil_restrictions/guardian_extension.rb
+++ lib/axiom_pupil_restrictions/guardian_extension.rb
@@ -3,12 +3,14 @@
 module ::AxiomPupilRestrictions
   module GuardianExtension
     def can_edit_username?(target_user)
+      return super if user&.staff?
       return false if ::AxiomPupilRestrictions.restricted_pupil?(target_user)
 
       super
     end
 
     def can_edit_name?(target_user)
+      return super if user&.staff?
       return false if ::AxiomPupilRestrictions.restricted_pupil?(target_user)
 
       super
